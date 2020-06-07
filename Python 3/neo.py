fff2c394bd257c3877157183c0458640ef3f870d
Mounir Afifi <mounir.afifi@intern.promatis.de>
2020-06-07 21:55:35 +0200

    adding two new regex solutions

---
+++ Python 3/neo.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+def word_connect(matrix):
+    ss = ''
+    for j in range(len(matrix[0])):
+        for i in range(len(matrix)):
+            ss+=matrix[i][j]
+    return ss
+
+first_multiple_input = input().rstrip().split()
+
+n = int(first_multiple_input[0])
+
+m = int(first_multiple_input[1])
+
+matrix = []
+
+for _ in range(n):
+    matrix_item = input()
+    matrix.append(matrix_item)
+
+pattern = r"(?<=[A-Za-z0-9])([^0-9a-zA-Z]+)(?=[A-Za-z0-9])"
+ss = word_connect(matrix)
+print(re.sub(pattern,' ',ss))
