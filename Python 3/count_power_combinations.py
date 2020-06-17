df460eb69e084f2629c8038241dc652a881e261b
Mounir Afifi <antontheog@gmail.com>
2020-06-17 13:59:56 +0200

    adding power_sum2 to calculate the combinations

--- Python 3/count_power_combinations.py
+++ Python 3/count_power_combinations.py
@@ -2,6 +2,11 @@
 
 import math
 
+sum_lists = []
+
+def append_list(lis, list_of_lists):
+    return [lis+x for x in list_of_lists]
+
 def powerSum(X, N, pos):
     if X == 1:
         if pos == 1:
@@ -27,6 +32,36 @@ def powerSum(X, N, pos):
             pos+=1
 
         return counter
+    
+def power_sum2(X,N,pos):
+    if X == 1:
+        if pos == 1:
+            return [[1]]
+        else:
+            return []
+    elif X in (2,3):
+        return []
+
+    sq = int(math.floor(X**(1.0/N)))+1
+    
+    power_sum_lists = []
+    while pos<=sq: 
+        if X - pos**N > 0:
+            #print("here "+str(n-fib_list_to_n[i]))
+            power_sum_lists += append_list([pos],power_sum2(X - pos**N,N,pos+1))
+
+        elif X - pos**N == 0:
+            #print("there")
+            power_sum_lists.append([pos])
+            
+            #print(fibo_sum_lists)
+        
+        
+        pos+=1   
+
+    
+    return power_sum_lists
+
 
 if __name__ == '__main__':
 
@@ -34,6 +69,11 @@ if __name__ == '__main__':
 
     N = int(input())
 
-    result = powerSum(X, N, 1)
+    #print(powerSum(X, N, 1))
+    power_sums = power_sum2(X, N, 1)
+    print(len(power_sums))
+   
+    for p in power_sums:
+        print(p)
+  
     
-    print(result)
