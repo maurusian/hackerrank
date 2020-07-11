f88e56ce181ba8f41f9969b2be05b859509bbc24
Mounir Afifi <antontheog@gmail.com>
2020-07-11 13:41:48 +0200

    adding query for listing perfect numbers

--- SQL/list_numbers.sql
+++ SQL/list_numbers.sql
@@ -13,3 +13,18 @@ FROM
 SELECT LEVEL a FROM DUAL CONNECT BY LEVEL < :p_number)
 WHERE :p_number/a = TRUNC(:p_number/a)
 /
+
+--list of perfect numbers up to :p_number
+SELECT LISTAGG(a,'.') WITHIN GROUP (ORDER BY a) PERFECT_LIST
+FROM
+(
+SELECT a
+    , (SELECT SUM(c) DSUM FROM (SELECT LEVEL c FROM DUAL CONNECT BY LEVEL<a) WHERE a/c = TRUNC(a/c)) DSUM
+FROM
+(
+SELECT LEVEL a FROM DUAL CONNECT BY LEVEL<=:p_number
+)
+)
+WHERE a = DSUM
+AND a>1
+/
